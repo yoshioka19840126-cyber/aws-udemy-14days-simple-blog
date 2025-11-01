dcc6addefec4f47a5f6a8d1409a003fe5e427185
hyoshioka <yoshioka.19840126@gmail.com>
2025-11-01 05:22:43 +0000

    update for code series.

--- src/index.php
+++ src/index.php
@@ -22,7 +22,7 @@
       }
     ?>
 
-    <h2 class="text-center mt-3 mb-5">Simple Blog Day13</h2>
+    <h2 class="text-center mt-3 mb-5">Simple Blog - Code Series</h2>
     <div class="container">
 
       <?php foreach ($posts as $post) : ?>
